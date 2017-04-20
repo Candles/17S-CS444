072dbcfdc55ff625277c6180a1e86b7681fec695
Candles <olsont@onid.orst.edu>
2017-04-19 21:58:23 -0700

    Fixed compilation errors; added command-line argument support

--- hw1/source/concurrency.c
+++ hw1/source/concurrency.c
@@ -3,13 +3,13 @@
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
+#include <getopt.h>
 
 #define NUM_CONSUMERS 5
 #define NUM_PRODUCERS 5
 #define NUM_JOBS 32
 
-pthread_t cons_ids[NUM_CONSUMERS];
-pthread_t prod_ids[NUM_PRODUCERS];
+
 pthread_mutex_t buffer_mutex;
 pthread_cond_t cond_empty;
 pthread_cond_t cond_full;
@@ -18,89 +18,133 @@ typedef struct Work {
 	int value, time;
 } Work;
 
-Work jobs[NUM_THREADS];
+Work jobs[NUM_JOBS];
 int jobs_count;
+void *consumer(void*);
+void *producer(void*);
 
-void consumer();
-void producer();
+int main( int argc, char *argv[]){
 
-int main(){
+	if (argc != 5){
+		goto invalidarg;
+	}
+
+	int num_consumers, num_producers;
+	char opt;
+	
+	while((opt = getopt(argc, argv, "c:p:")) != -1){
+		switch(opt){
+		case 'c':
+			num_consumers = atoi(optarg);
+			if (num_consumers < 1){
+				printf("Warning: No consumer threads specified\n");
+			}
+			break;
+		case 'p':
+			num_producers = atoi(optarg);
+			if (num_producers < 1){
+				printf("Warning: No producer threads specified\n");
+			}
+			break;
+		default:
+			goto invalidarg;
+		}
+	}
+	
+	if(0){
+		invalidarg:
+		printf("Usage: %s -c <consumer_threads> -p <producer_threads>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	
 
 	srand(time(NULL));
 
-	pthread_mutex_init(&mutex, NULL);
+	pthread_t cons_ids[num_consumers];
+	pthread_t prod_ids[num_producers];
+	pthread_mutex_init(&buffer_mutex, NULL);
 	pthread_cond_init(&cond_empty, NULL);
 	pthread_cond_init(&cond_full, NULL);
 	jobs_count = 0;
+
 	
-	for(i = 0; i++; i < NUM_CONSUMERS){
-		pthread_create(cons_ids[i], NULL, *consumer, NULL);
+	for(int i = 0; i < num_consumers; i++){
+		pthread_create(&cons_ids[i], NULL, consumer, NULL);
 	}
 
-	for(i = 0; i++; i < NUM_PRODUCERS){
-		pthread_create(prod_ids[i], NULL, *producer, NULL);
+	for(int i = 0; i < num_producers; i++){
+		pthread_create(&prod_ids[i], NULL, producer, NULL);
 	}
-	
-	while(1); //busy loop
-	
+
+
+	while(1){ sleep(10); } //busy loop
+
 	return 0;
 }
 
-void consumer(){
+void *consumer(void *args){
 	while(1){
+
+
 		//LOCK BUFFER
 		pthread_mutex_lock(&buffer_mutex);
-		
+
 		//WAIT FOR JOB
 		while(jobs_count == 0){
 			pthread_cond_wait(&cond_empty, &buffer_mutex);
 		}
-		
+
 		//REMOVE JOB
-		jobs_count--;
-		
+		Work job = jobs[--jobs_count];
+
 		//Broadcast to blocked threads that there's space in the buffer
 		if(jobs_count == NUM_JOBS - 1){
 			pthread_cond_broadcast(&cond_full);
 		}
-		
+
 		//UNLOCK BUFFER
 		pthread_mutex_unlock(&buffer_mutex);
-		
+
 		//PROCESS JOB
-		sleep(job->time);
-		printf("%d\n", job->value);
+		sleep(job.time);
+		printf("Value: %d\n", job.value);
+
+	}
 }
 
-void producer(){
+void *producer(void *args){
 	while(1){
 		//CREATE WORK
-		Work *job = malloc(sizeof(Work));
-		job->value = rand() % 10;
-		job->time = rand() % 7 + 2;
-		
+		Work job;
+		job.value = rand() % 10;
+		job.time = rand() % 7 + 2;
+
+
 		//LOCK BUFFER
 		pthread_mutex_lock(&buffer_mutex);
-		
+
 		//WAIT FOR SPACE
 		while(jobs_count == NUM_JOBS){
 			pthread_cond_wait(&cond_full, &buffer_mutex);
 		}
-		
+
 		//ADD WORK
 		jobs[jobs_count++] = job;
-		
+
+
+
 		//If we just added a job, broadcast to all blocked threads that there's work
 		if(jobs_count == 1){
 			pthread_cond_broadcast(&cond_empty);
 		}
-		
+
 		//UNLOCK BUFFER
 		pthread_mutex_unlock(&buffer_mutex);
-		
+
 		sleep(rand() % 5 + 3);
 	}
 }
+
 /*
 int rand(){
 	int val, success;
@@ -111,8 +155,7 @@ int rand(){
 			: "=r" (val), "=g" (success)
 		);
 	} while(!success);
-	
-	
+
 	return val;
 }
 */
