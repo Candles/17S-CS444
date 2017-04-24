a78dcc0e62facd2566375b45c1eff1cf075367af
Candles <olsont@onid.orst.edu>
2017-04-24 11:38:35 -0700

    Initial commit for Synchro2 and HW2

---
+++ synchro2/concurrency.c
@@ -0,0 +1,48 @@
+//======================================================//
+//Spring 2017 CS-444                                    //
+//Assignment #2                                         //
+//                                                      //
+//                                                      //
+//By Chongxian Chen, Thomas Olson and Christopher Tang	//
+//======================================================//
+
+#include "stdio.h"
+#include "pthread.h"
+#include "unistd.h"
+#include "time.h"
+#include "stdlib.h"
+
+void think();
+void get_forks();
+void eat();
+void put_forks();
+void *philosopher(void*);
+
+
+
+int main( int argc, char *argv[]){
+
+	while(1){ sleep(10); } //busy loop
+
+	return 0;
+}
+
+void think(){
+
+}
+
+void get_forks(){
+
+}
+
+void eat(){
+
+}
+
+void put_forks(){
+
+}
+
+void *philosopher(void* arg){
+
+}
