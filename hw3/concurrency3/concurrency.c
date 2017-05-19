830f5cd218af86fd0cf2b43f94350bdb4aac58e7
Chril <crazypizzak2@gmail.com>
2017-05-19 00:19:48 -0700

    Concurrency without linked list

--- hw3/concurrency3/concurrency.c
+++ hw3/concurrency3/concurrency.c
@@ -52,17 +52,65 @@
 #define LIST_HEAD(listhead)
 struct list_head listhead = LIST_HEAD_INIT(listhead)
 
-sem_t list_lock_modify; //lock if ANY changes are being made
-sem_t list_lock_delete;	//lock if you are a deleter and currently deleting
 
+typedef struct 
+{
+    int counter;
+    sem_t mutex;
+
+}LightSwitch;
+
+/**
+ * @brief      Multi-thread semaphore controller - LOCK
+ *
+ * @param      ls    Pointer to LightSwitch struct
+ * @param      sem   The semaphore to be locked
+ */
+void ls_lock(LightSwitch *ls, sem_t *sem){
+    sem_wait(&ls->mutex);
+    ls->counter++;
+    if (ls->counter == 1)
+    {
+        sem_wait(sem);
+    }
+    sem_post(&ls->mutex);
+}
+
+
+/**
+ * @brief      Multi-thread semaphore controller - UNLOCK
+ *
+ * @param      ls    Pointer to LightSwitch struct
+ * @param      sem   The semaphore to be unlocked
+ */
+void ls_unlock(LightSwitch *ls, sem_t* sem){
+    sem_wait(&ls->mutex);
+    ls->counter--;
+    if (ls->counter == 0)
+    {
+         sem_post(sem);
+    }
+    sem_post(&ls->mut);
+}
+
+
+sem_t insertMutex;
+sem_t noSearcher;
+sem_t noInserter;
+
+struct LightSwitch searchSwitch;
+struct LightSwitch insertSwitch;
 
 int main( int argc, char *argv[]){
 	int i = 0;
 	char name[9];
 	int worker[MAXTHREADS];
 	pthread_t worker_thread[MAXTHREADS];
-	sem_init(&list_lock_modify, 0, 0);
-	sem_init(&list_lock_delete, 0, 0);
+	
+	sem_init(&insertMutex, 0, 1);
+	sem_init(&noSearcher, 0, 1);
+	sem_init(&noInserter, 0, 1);
+
 
 	INIT_LIST_HEAD(*listhead);
 
@@ -91,7 +139,11 @@ int main( int argc, char *argv[]){
     return 0;
 }
 
-
+/**
+ * @brief      Directs threads to their designated functions.
+ *
+ * @param      arg   Worker objective
+ */
 void *wrok(void* arg){
     int objective = *((int*)arg);
     while(1){
@@ -109,29 +161,36 @@ void *wrok(void* arg){
 	}//while
 }//work
 
-//needs a way to tell if delete is running or not
+/**
+ * @brief      Locks the noSearcher sem with the ability to add more searchers.
+ * 
+ */
 void search(){
-	sval_t temp = 0;
-
-	if (sem_trywait(list_lock_delete) == 0){
-		printf("looking for a thing");
-	} else {		//deleter active
-	       		 	//don't search
-	}
+	ls_lock(searchSwitch, noSearcher);
+	printf("Searching for something!\n");
+	printf("Done searching\n");
+	ls_unlock(searchSwitch, noSearcher);
 
 }
 
+/**
+ * @brief      Locks noInserter sem then blocks until it can grab inserMutex.
+ */
 void insert(){
-	if (sem_trywait(list_lock_modify) == 0){
-		struct list_head *new = malloc(sizeof(struct list_head));
-		list_add_tail(new,listhead);
-		sem_post(list_lock_modify);
-	}
+	ls_lock(insertSwitch, noInserter);
+	sem_wait(insertMutex); 				//wait for other inserts to finish
+	printf("Inserting!\n");
+	sem_post(insertMutex);
+	ls_unlock(insertSwitch, noInserter);
 }
 
+/**
+ * @brief      Waits for noSearcher and noInsert before deleting.
+ */
 void delete(){
-	if (sem_trywait(list_lock_modify)){		// insert isn't inserting or another deleter isn't deleting
-		sem_wait(list_lock_delete);			// no search running
-
-	}
+	sem_wait(noSearcher);
+	sem_wait(noInserter);
+	printf("Deleting!\n");
+	sem_post(noInserter);
+	sem_post(noSearcher);
 }
